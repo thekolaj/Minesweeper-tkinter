bd9f3a252628d75d777a9697efdb5d8887e037aa
KolaJ <thekolaj@gmail.com>
2023-09-26 14:37:43 +0300

    Speed test created

---
+++ speed_test.py
@@ -0,0 +1,15 @@
+"""See how much time each function takes by running the game through this file
+Jus execute this file as if it was main.py.
+"""
+import cProfile
+import main
+
+def test():
+    pr = cProfile.Profile()
+    pr.enable()
+    main.main()
+    pr.disable()
+    pr.print_stats(sort='time')
+
+if __name__ == "__main__":
+    test()
